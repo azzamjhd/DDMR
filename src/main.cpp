3da3927d911b01add8a1487d2ef2a618de12d01e
azzamjhd <71210715+azzamjhd@users.noreply.github.com>
2024-09-12 19:35:54 +0700

    Refactor motor control code and add encoder functionality

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,38 @@
 #include "pins.h"
 
 int speed = 0;
+bool isForward = true;
 
-#define Ultrasonic1 12
+// Encoder and Motor objects
+Encoder rightEncoder(R_ENC_A, R_ENC_B);
+Encoder leftEncoder(L_ENC_A, L_ENC_B);
+Motor rightMotor(PWM_A, A_IN_1, A_IN_2);
+Motor leftMotor(PWM_B, B_IN_1, B_IN_2);
 
 void setup() {
     Serial.begin(9600);
-    setupMotorPins();
+    attachInterrupt(digitalPinToInterrupt(R_ENC_A), []() { rightEncoder.update(); }, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(L_ENC_A), []() { leftEncoder.update(); }, CHANGE);
 }
 
 void loop() {
-    speed++;
-    Serial.println(speed);
-    setMotorDir('R', 'F');
-    setMotorDir('L', 'F');
-    analogWrite(PWM_A, speed%255);
-    analogWrite(PWM_B, speed%255);
-    delay(100);
+    if (speed < 256 && isForward) {
+        speed++;
+    } else if ( speed < 256 && !isForward) {
+        speed--;
+    }
+
+    if (speed == 255) {
+        isForward = false;
+        rightMotor.setDir(STOP);
+    } else if (speed == 0) {
+        isForward = true;
+    }
+
+    Serial.print("Speed: "); Serial.println(speed);
+    Serial.print("Right Encoder: "); Serial.println(rightEncoder.getCount());
+    Serial.print("Left Encoder: "); Serial.println(leftEncoder.getCount());
+
+    rightMotor.setSpeed(speed);
+    leftMotor.setSpeed(speed);
 }
--- src/pins.h
+++ src/pins.h
@@ -11,70 +11,113 @@
 #define ECHO_PIN_3 42
 #define TRIG_PIN_3 44
 
+// ==================== DC Motor with TB6612FNG Driver ====================
+// inA = attached to Right motor, inB = attached to Left motor
+
 // Rotary Encode Motor
+#define FORWARD 0b01
+#define BACKWARD 0b10
+#define STOP 0b00
+
+// Right Motor Encoder A pin
 #define R_ENC_A 3
 #define R_ENC_B 5
+// Left Motor Encoder A pin
 #define L_ENC_A 2
 #define L_ENC_B 4
 
-// Driver Motor TB6612FNG
-// A = Right, B = Left
+
+// Right motor PWM pin
 #define PWM_A 11
+// Right motor IN1
 #define A_IN_2 10
+// Right motor IN2
 #define A_IN_1 9
+// Left motor IN1
 #define B_IN_1 8
+// Left motor IN2
 #define B_IN_2 7
+// Left motor PWM pin
 #define PWM_B 6
 
 // MPU6050
 // INT - GND - SDA - SCL - GND - VCC
+
+// Interrupt pin
 #define INT 19
+// MPU6050 SDA pin
 #define SDA 20
+// MPU6050 SCL pin
 #define SCL 21
 
-void setupMotorPins() {
-  pinMode(PWM_A, OUTPUT);
-  pinMode(A_IN_1, OUTPUT);
-  pinMode(A_IN_2, OUTPUT);
-  pinMode(PWM_B, OUTPUT);
-  pinMode(B_IN_1, OUTPUT);
-  pinMode(B_IN_2, OUTPUT);
-  Serial.println("Motor pins setup");
-}
 
-void setMotorDir(char motor, char dir) {
-    switch(motor) {
-        case 'R':
-            if (dir == 'F') {
-                digitalWrite(A_IN_1, HIGH);
-                digitalWrite(A_IN_2, LOW);
-                Serial.println("Right motor moving forward");
-            } else if (dir == 'B') {
-                digitalWrite(A_IN_1, LOW);
-                digitalWrite(A_IN_2, HIGH);
-                Serial.println("Right motor moving backward");
-            } else {
-                digitalWrite(A_IN_1, LOW);
-                digitalWrite(A_IN_2, LOW);
-                Serial.println("Right motor stopped");
-            }
-            break;
-        case 'L':
-            if (dir == 'F') {
-                digitalWrite(B_IN_1, HIGH);
-                digitalWrite(B_IN_2, LOW);
-                Serial.println("Left motor moving forward");
-            } else if (dir == 'B') {
-                digitalWrite(B_IN_1, LOW);
-                digitalWrite(B_IN_2, HIGH);
-                Serial.println("Left motor moving backward");
-            } else {
-                digitalWrite(B_IN_1, LOW);
-                digitalWrite(B_IN_2, LOW);
-                Serial.println("Left motor stopped");
-            }
-            break;
+class Encoder {
+private:
+    int pinA;
+    int pinB;
+    int count;
+    uint8_t curretState, lastState;
+
+public:
+    Encoder(int pinA, int pinB) {
+        this->pinA = pinA;
+        this->pinB = pinB;
+        this->count = 0;
+        this->curretState = 0;
+        this->lastState = 0;
+        pinMode(pinA, INPUT);
+        pinMode(pinB, INPUT);
     }
-}
+
+    void update() {
+        this->curretState = digitalRead(this->pinA) | digitalRead(this->pinB) << 1;
+        if (this->curretState == 0b10 && this->lastState == 0b01) {
+            this->count++;
+        } else if (this->curretState == 0b11 && this->lastState == 0b00) {
+            this->count--;
+        }
+    }
+
+    int getCount() {
+        return this->count;
+    }
+};
+
+class Motor {
+private:
+    int pwm;
+    int in1;
+    int in2;
+public:
+    Motor(int pwm, int in1, int in2) {
+        this->pwm = pwm;
+        this->in1 = in1;
+        this->in2 = in2;
+        pinMode(pwm, OUTPUT);
+        pinMode(in1, OUTPUT);
+        pinMode(in2, OUTPUT);
+    }
+
+    void setSpeed(int speed) {
+        analogWrite(this->pwm, speed);
+    }
+
+    void setDir(uint8_t dir) {
+        switch (dir) {
+            case FORWARD:
+                digitalWrite(this->in1, LOW);
+                digitalWrite(this->in2, HIGH);
+                break;
+            case BACKWARD:
+                digitalWrite(this->in1, HIGH);
+                digitalWrite(this->in2, LOW);
+                break;
+            case STOP:
+                digitalWrite(this->in1, LOW);
+                digitalWrite(this->in2, LOW);
+                break;
+        }
+    }
+};
 
 #endif // PINS_H_
